db523d9b54dafbf5e628d8afcc402ee00fc64978
FISHCAT8 <82520225+Doughet@users.noreply.github.com>
2024-05-09 17:12:01 +0200

    Reading some input

--- BipartiteKnapsackProblem/src/Main.java
+++ BipartiteKnapsackProblem/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //GET THE DATA UNPROCESSED
+        BipartiteDataProcessing dataProcessing = new BipartiteDataProcessing();
+        ArrayList<String> dataUnprocessed = dataProcessing.getUnprocessedObjectDescriptions();
+        BipartiteDataProcessing.ParsedInput parsedInput = dataProcessing.parseObjectLines(dataUnprocessed, dataUnprocessed.size());
+
+        //System.out.println(Arrays.toString(parsedInput.objectCodes()));
+        //System.out.println(Arrays.deepToString(parsedInput.objectDescriptions()));
     }
+
+
+
+
 }
